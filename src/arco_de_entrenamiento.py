135ff3012a387175d9b5abb95dd36b3ba5bd53ec
vq0001 <vquiroga2023@udec.cl>
2025-12-11 22:39:38 -0300

    fix: Arreglar nombre de funcion porcentaje_explorado en torneos_de_entrenamiento

--- src/arco_de_entrenamiento.py
+++ src/arco_de_entrenamiento.py
@@ -19,31 +19,11 @@ class Metrica(enum.Enum):
     EXPLORACION = 1
 
 # ejecutar torneos de entrenamiento para una estrategia dada, y guardar las metricas solicitadas
-def torneos_de_entrenamiento(estrategia, metrica_a_guardar, episodios_por_torneo=100, cantidad_de_torneos=100, guardado_cada_n_torneos=10):
-
-    estategias_a_enfrentar = [
-        TitForTat(),
-        SiempreCoopera(),
-        SiempreTraiciona(),
-        Random(),
-        Davis(),
-        Downing(),
-        Feld(),
-        Grofman(),
-        Joss(),
-        Shubik(),
-        SteinRapoport(),
-        TidemanChieruzzi(),
-        Tullock(),
-        Nydegger(),
-        Graaskamp(),
-        Grudger(),
-        Anonymous()
-    ]
+def torneos_de_entrenamiento(estrategia, estrategias_a_enfrentar, metrica_a_guardar, episodios_por_torneo=100, cantidad_de_torneos=100, guardado_cada_n_torneos=10):
 
     # quitar estrategia de la lista si ya esta
-    if estrategia.__class__.__name__ in estategias_a_enfrentar:
-        estategias_a_enfrentar.remove(estrategia.__class__.__name__)
+    if estrategia.__class__.__name__ in estrategias_a_enfrentar:
+        estrategias_a_enfrentar.remove(estrategia.__class__.__name__)
 
     df = pd.DataFrame(columns=["estrategia", "n_torneo", "perdida", "exploracion", "puntaje_torneo", "puntaje_acumulado"])
 
@@ -52,10 +32,8 @@ def torneos_de_entrenamiento(estrategia, metrica_a_guardar, episodios_por_torneo
     # ejecutar los torneos 1 contra todos
     for torneo in range(cantidad_de_torneos):
 
-        print(f"Iniciando torneo {torneo + 1}/{cantidad_de_torneos} para {estrategia.__class__.__name__}")
-
         controlador = ControladorDuelos(
-            estrategias_a_enfrentar=estategias_a_enfrentar,
+            estrategias_a_enfrentar=estrategias_a_enfrentar,
             cantidad_de_torneos=1,
             jugadas_base_duelo=episodios_por_torneo,
             limite_de_variacion_de_jugadas=0,
@@ -64,6 +42,8 @@ def torneos_de_entrenamiento(estrategia, metrica_a_guardar, episodios_por_torneo
         controlador.iniciar_duelos(analisis=False, verbose=True)
         puntaje_acumulado += estrategia.puntaje_torneo_actual
 
+
+        # Guardar las métricas cada n torneos
         if (torneo + 1) % guardado_cada_n_torneos == 0:
             fila = {"estrategia": estrategia.__class__.__name__,
                     "n_torneo": torneo + 1,
@@ -76,7 +56,7 @@ def torneos_de_entrenamiento(estrategia, metrica_a_guardar, episodios_por_torneo
                 if Metrica.PERDIDA == metrica_a_guardar:
                     fila["perdida"] = estrategia.get_loss()
                 elif Metrica.EXPLORACION == metrica_a_guardar:
-                    fila["exploracion"] = estrategia.get_exploration()
+                    fila["exploracion"] = estrategia.porcentaje_explorado()
             except AttributeError:
                 # La estrategia no tiene la métrica solicitada: saltar agregación
                 print("La estrategia no tiene la métrica solicitada.")
@@ -97,20 +77,29 @@ if __name__ == "__main__":
     #random.seed(42)
 
     cantidad_de_torneos = 5
+    guardado_cada_n_torneos = 1
     jugadas_base_duelo = 100
     limite_de_variacion_de_jugadas = 10
 
-    # estrategias = [
-    #     TitForTat(),
-    #     SiempreCoopera(),
-    #     SiempreTraiciona(),
-    #     Random(),
-    #     Davis(),
-    #     Downing(),
-    #     Feld(),
-    #     Grofman(),
-    #     Joss(),
-    # ]
+    estrategias_a_enfrentar = [
+        TitForTat(),
+        SiempreCoopera(),
+        SiempreTraiciona(),
+        Random(),
+        Davis(),
+        Downing(),
+        Feld(),
+        Grofman(),
+        Joss(),
+        Shubik(),
+        SteinRapoport(),
+        TidemanChieruzzi(),
+        Tullock(),
+        Nydegger(),
+        Graaskamp(),
+        Grudger(),
+        Anonymous()
+    ]
 
     protas = [
         [DeepQNetwork(
@@ -122,7 +111,6 @@ if __name__ == "__main__":
         [A2C(), Metrica.PERDIDA],
         [DuelingDQN(), Metrica.PERDIDA],
         [A2C_LSTM(), Metrica.PERDIDA],
-
     ]
 
     dataf = pd.DataFrame()
@@ -142,9 +130,10 @@ if __name__ == "__main__":
 
         resultado = torneos_de_entrenamiento(
             prota,
+            estrategias_a_enfrentar,
             metrica, 
             cantidad_de_torneos=cantidad_de_torneos,
-            guardado_cada_n_torneos=1
+            guardado_cada_n_torneos=guardado_cada_n_torneos
         )
             
         print(resultado)
@@ -153,5 +142,4 @@ if __name__ == "__main__":
 
        # prota.export_QTable(f"{prota.__class__.__name__}+TFT2")
 
-
-    print(dataf)
+    dataf.to_csv("resultados_arco_de_entrenamiento.csv", index=False)
